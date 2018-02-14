f955e0356fc5ebbfef9cfd287b14f51cecd8259b
Manuel Marcatili <manuel.marcatili@globallogic.com>
2018-02-14 13:56:58 -0300

    added python example

---
+++ lootboxesAreGambling/python/Game.py
@@ -0,0 +1,18 @@
+import argparse
+import random
+
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('integers', metavar='N', type=int, nargs='+',
+                    help='an integer for the accumulator')
+parser.add_argument('--sum', dest='accumulate', action='store_const',
+                    const=sum, default=max,
+                    help='sum the integers (default: find the max)')
+
+args = parser.parse_args()
+
+results = []
+
+for dice in args.integers:
+    results.append(random.randint(1, dice))
+
+print(results)
